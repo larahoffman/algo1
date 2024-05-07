685a944be1e1cba9435a50d2eaaa342ed783bd1a
larahoffman <larahoff02@gmail.com>
2024-05-07 19:48:50 -0300

    ejercicio 3

--- parcialA.hs
+++ parcialA.hs
@@ -13,3 +13,18 @@ cantidadApariciones :: Char -> [Char] -> Int
 cantidadApariciones _ [] = 0
 cantidadApariciones c (f1:f2) | c == f1 = 1 + cantidadApariciones c f2
                               | otherwise = cantidadApariciones c f2
+
+-- Ej 3
+laQueMasHayQueCodificar :: [Char] -> [(Char, Char)] -> Char
+laQueMasHayQueCodificar frase mapeo = laQueMasHayQueCodificarAux frase frase mapeo
+
+laQueMasHayQueCodificarAux :: [Char] -> [Char] -> [(Char, Char)] -> Char
+laQueMasHayQueCodificarAux [f] _ _ = f
+laQueMasHayQueCodificarAux (f1:f2:fs) fraseOriginal mapeo
+    | f1 == f2 = laQueMasHayQueCodificarAux (f1:fs) fraseOriginal mapeo
+    | cuantasVecesHayQueCodificar f1 fraseOriginal mapeo >= cuantasVecesHayQueCodificar f2 fraseOriginal mapeo = laQueMasHayQueCodificarAux (f1:fs) fraseOriginal mapeo
+    | otherwise = laQueMasHayQueCodificarAux (f2:fs) fraseOriginal mapeo
+
+-- Ej 4
+--codificarFrase :: [Char] -> [(Char, Char)] -> [Char]
+--
